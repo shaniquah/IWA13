8daeaebff6ac6baf8110313ccb505a4779adcaf8
Otshepeng Setuke <shaniquah.setuke@gmail.com>
2023-05-12 10:10:22 +0200

    Create script.js

---
+++ challenge-1/script.js
@@ -0,0 +1,42 @@
+
+let state = 'idle'
+let user = null
+let calculated = '1'
+
+// Only allowed to change below
+
+
+const logCalc = () => { 
+    calculated = parseInt(calculated) + 2
+    return calculated;
+};
+
+const calcUser = () => {
+  logCalc()
+  if (calculated > 2) { user = 'John' }
+  if (calculated > 2) { state = 'requesting' }
+  if (calculated > 3) { state = 'idle' }
+}
+
+const  checkUser = () => {
+	if (user === 'John' && state === 'requesting') {
+		console.log(`User: ${user} (${calculated})`)
+	}
+}
+
+// Only allowed to change code above
+
+checkUser()
+calcUser()
+
+checkUser()
+calcUser()
+
+checkUser()
+calcUser()
+
+checkUser()
+calcUser()
+
+checkUser()
+calcUser()
